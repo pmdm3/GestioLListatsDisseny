f5e27c4947ebec0c128e5c26b065a4c0bc3bcad3
Federico <Federico@EasyNote>
2014-01-08 18:05:41 +0100

    throw format fitxer

--- src/Model/Llegir.java
+++ src/Model/Llegir.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Llegir {
 
-    public ArrayList<String[]> llegir(String path) {
+    public ArrayList<String[]> llegir(String path) throws Exception{
         File fitxer;
         //Creació buffer per la lectura
         BufferedReader br = null;
@@ -24,12 +24,20 @@ public class Llegir {
             //Tractament de les lines del fitxer
             String linea = br.readLine();
             while (linea != null) {
-                            //Comprovar que les linies no són comentaris
+                //Comprovar que les linies no són comentaris
                 //En cas afirmatiu eliminar caracters
                 //Traspas de la linea a la estructura a tractar (dades)
                 if (!linea.startsWith(" #")) {
                     linea = linea.replace("\"", "");
                     dades.add(linea.split(","));
+                } else {
+                    String[]cap = new String[4];
+                    linea = linea.replace("\"", "");
+                    cap = linea.split(",");
+                    if(cap[1].compareTo("00_NOM")!=0 || cap[2].compareTo("01_GRUPSCLASSE")!=0 || cap[3].compareTo("02_MATRICULADES")!=0)
+                    {
+                        throw new Exception("el fitxer no té el format correcte");   
+                    }
                 }
                 //Següent linea
                 linea = br.readLine();
