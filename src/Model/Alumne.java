2829ede2eb407f838ee961666b6d525341b2c6c2
Federico <Federico@EasyNote>
2014-01-08 11:46:30 +0100

    indentació

--- src/Model/Alumne.java
+++ src/Model/Alumne.java
@@ -1,74 +1,70 @@
 package Model;
 
-
 import java.util.TreeSet;
 
 public class Alumne implements Comparable<Alumne> {
-	private String cognoms, nom, grup;
-	private TreeSet<String> materia;
-	
-	public Alumne() {
-		this.cognoms = "";
-		this.nom = "";
-		this.grup = "";
-		this.materia = null;
-	}
 
+    private String cognoms, nom, grup;
+    private TreeSet<String> materia;
 
-	public Alumne(String cognoms, String nom, String grup, TreeSet<String> materia) {
-		super();
-		this.cognoms = cognoms;
-		this.nom = nom;
-		this.grup = grup;
-		this.materia = materia;
-	}
+    public Alumne() {
+        this.cognoms = "";
+        this.nom = "";
+        this.grup = "";
+        this.materia = null;
+    }
 
-	public TreeSet<String> getMateria() {
-		return materia;
-	}
+    public Alumne(String cognoms, String nom, String grup, TreeSet<String> materia) {
+        super();
+        this.cognoms = cognoms;
+        this.nom = nom;
+        this.grup = grup;
+        this.materia = materia;
+    }
 
+    public TreeSet<String> getMateria() {
+        return materia;
+    }
 
-	public void setMateria(TreeSet<String> materia) {
-		this.materia = materia;
-	}
+    public void setMateria(TreeSet<String> materia) {
+        this.materia = materia;
+    }
 
+    public String getCognoms() {
+        return cognoms;
+    }
 
-	public String getCognoms() {
-		return cognoms;
-	}
+    public void setCognoms(String cognomsNom) {
+        this.cognoms = cognomsNom;
+    }
 
-	public void setCognoms(String cognomsNom) {
-		this.cognoms = cognomsNom;
-	}
-	
-	public String getNom() {
-		return nom;
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	public String getGrup() {
-		return grup;
-	}
+    public String getGrup() {
+        return grup;
+    }
 
-	public void setGrup(String grup) {
-		this.grup = grup;
-	}
-	
-	@Override
-	public String toString() {
-		return "Alumne cognoms=" + cognoms + ", nom=" + nom + ", grup=" + grup
-				+ ", materia=" + materia + "\n";
-	}
+    public void setGrup(String grup) {
+        this.grup = grup;
+    }
 
-	@Override
-	public int compareTo(Alumne o) {
-            
-            int valor = this.grup.compareToIgnoreCase(o.getGrup());
-            return valor== 0? this.cognoms.compareToIgnoreCase(o.getCognoms()):valor;
-                
-                
-	}
+    @Override
+    public String toString() {
+        return "Alumne cognoms=" + cognoms + ", nom=" + nom + ", grup=" + grup
+                + ", materia=" + materia + "\n";
+    }
+
+    @Override
+    public int compareTo(Alumne o) {
+
+        int valor = this.grup.compareToIgnoreCase(o.getGrup());
+        return valor == 0 ? this.cognoms.compareToIgnoreCase(o.getCognoms()) : valor;
+
+    }
 }
--- src/Model/Llegir.java
+++ src/Model/Llegir.java
@@ -6,43 +6,43 @@ import java.util.ArrayList;
 
 public class Llegir {
 
-	public ArrayList<String[]> llegir(String path) {
-		File fitxer;
-                //Creació buffer per la lectura
-		BufferedReader br = null;
-		ArrayList<String[]> dades = new ArrayList<String[]>();
+    public ArrayList<String[]> llegir(String path) {
+        File fitxer;
+        //Creació buffer per la lectura
+        BufferedReader br = null;
+        ArrayList<String[]> dades = new ArrayList<String[]>();
 
-		try {
-                    //Creació fitxer amb el path indicat
-			fitxer = new File(path);
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(fitxer), Charset.forName("ISO-8859-15")));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+        try {
+            //Creació fitxer amb el path indicat
+            fitxer = new File(path);
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(fitxer), Charset.forName("ISO-8859-15")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-		try {
-                    //Tractament de les lines del fitxer
-			String linea = br.readLine();
-			while (linea != null) {
+        try {
+            //Tractament de les lines del fitxer
+            String linea = br.readLine();
+            while (linea != null) {
                             //Comprovar que les linies no són comentaris
-                            //En cas afirmatiu eliminar caracters
-                            //Traspas de la linea a la estructura a tractar (dades)
-				if (!linea.startsWith(" #")) {
-					linea = linea.replace("\"","");
-					dades.add(linea.split(","));
-				}
-                                //Següent linea
-				linea = br.readLine();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return dades;
-	}
+                //En cas afirmatiu eliminar caracters
+                //Traspas de la linea a la estructura a tractar (dades)
+                if (!linea.startsWith(" #")) {
+                    linea = linea.replace("\"", "");
+                    dades.add(linea.split(","));
+                }
+                //Següent linea
+                linea = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return dades;
+    }
 }
